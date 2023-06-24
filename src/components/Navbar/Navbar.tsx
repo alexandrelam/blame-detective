37e30952ba96ebc05f5ea7f7981db33c40e04e4d
Alexandre Lam <alexandre.lam@doctolib.com>
2023-06-24 11:59:15 +0200

    added title + new moving icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,12 +8,25 @@ type Props = {
 export function NavBar({ tab, setTab }: Props) {
   return (
     <div className="py-6 border-b flex justify-between items-center">
-      <h1
-        className="font-bold text-2xl text-primary cursor-pointer"
-        onClick={() => setTab("search")}
-      >
-        🕵️ &nbsp; GitHunt
-      </h1>
+      <div className="flex gap-2 items-center">
+        <img
+          src="/src/assets/detective.png"
+          alt="moving detective microsft emoji"
+          className="w-12 h-12"
+        />
+        <div>
+          <h1
+            className="font-bold text-2xl text-primary cursor-pointer"
+            onClick={() => setTab("search")}
+          >
+            B<span className="italic text-secondary-focus font-serif">lam</span>
+            e Detective
+          </h1>
+          <span className="text-sm text-base-content/70 block -mt-1">
+            Empowering Developers to Track and Expose Code Alterations!
+          </span>
+        </div>
+      </div>
       <div className="tabs tabs-boxed">
         <a
           className={`tab ${tab === "search" && "tab-active"}`}
