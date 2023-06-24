1e2ba184a5a27978bfd7d806c04549f139e96c45
Alexandre Lam <alexandre.lam@doctolib.com>
2023-06-24 23:06:10 +0200

    images for production

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -13,7 +13,7 @@ export function NavBar({ tab, setTab }: Props) {
         onClick={() => setTab("search")}
       >
         <img
-          src="/src/assets/detective.png"
+          src="blame-detective/public/detective.png"
           alt="moving detective microsft emoji"
           className="w-12 h-12"
         />
