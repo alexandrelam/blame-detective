efc977ea458c07856e54e06a27d1e8f399991797
Alexandre Lam <alexandre.lam@doctolib.com>
2023-06-24 16:00:25 +0200

    :broom: removed unused code

--- src/utils/useGithubStats.ts
+++ src/utils/useGithubStats.ts
@@ -3,14 +3,10 @@ import { GithubAPIRate } from "../types";
 
 export function useGithubStats() {
   const [data, setData] = useState<GithubAPIRate | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isError, setIsError] = useState<boolean>(false);
 
   async function fetchGithubStats(token: string) {
     if (!token) return console.log("missing token");
     try {
-      setIsError(false);
-      setIsLoading(true);
       const res = await fetch("https://api.github.com/rate_limit", {
         method: "GET",
         headers: {
@@ -21,12 +17,10 @@ export function useGithubStats() {
 
       const data = await res.json();
       setData(data.rate);
-      setIsLoading(false);
     } catch (error) {
-      setIsError(true);
-      setIsLoading(false);
+      console.log(error);
     }
   }
 
-  return { data, isLoading, isError, fetchGithubStats };
+  return { data, fetchGithubStats };
 }
