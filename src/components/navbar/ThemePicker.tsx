a6b65cd6db1c8fb071b6991d1dfc10848c28e8ab
Alexandre Lam <alexandre.lam@doctolib.com>
2023-06-24 22:41:42 +0200

    Restore folder name to original

---
+++ src/components/navbar/ThemePicker.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+
+export function ThemePicker() {
+  const [selectedTheme, setSelectedTheme] = useState("");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("selectedTheme");
+    if (savedTheme) {
+      setSelectedTheme(savedTheme);
+      document.documentElement.setAttribute("data-theme", savedTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", selectedTheme);
+  }, [selectedTheme]);
+
+  const handleThemeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const theme = event.target.value;
+    setSelectedTheme(theme);
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("selectedTheme", theme);
+  };
+
+  return (
+    <div className="flex gap-2 items-center">
+      <label htmlFor="themeSelect">Select a theme:</label>
+      <select
+        id="themeSelect"
+        className="input input-bordered"
+        value={selectedTheme}
+        onChange={handleThemeChange}
+      >
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="cupcake">Cupcake</option>
+        <option value="bumblebee">Bumblebee</option>
+        <option value="emerald">Emerald</option>
+        <option value="corporate">Corporate</option>
+        <option value="synthwave">Synthwave</option>
+        <option value="retro">Retro</option>
+        <option value="cyberpunk">Cyberpunk</option>
+        <option value="valentine">Valentine</option>
+        <option value="halloween">Halloween</option>
+        <option value="garden">Garden</option>
+        <option value="forest">Forest</option>
+        <option value="aqua">Aqua</option>
+        <option value="lofi">Lo-fi</option>
+        <option value="pastel">Pastel</option>
+        <option value="fantasy">Fantasy</option>
+        <option value="wireframe">Wireframe</option>
+        <option value="black">Black</option>
+        <option value="luxury">Luxury</option>
+        <option value="dracula">Dracula</option>
+        <option value="cmyk">CMYK</option>
+        <option value="autumn">Autumn</option>
+        <option value="business">Business</option>
+        <option value="acid">Acid</option>
+        <option value="lemonade">Lemonade</option>
+        <option value="night">Night</option>
+        <option value="coffee">Coffee</option>
+        <option value="winter">Winter</option>
+      </select>
+    </div>
+  );
+}
