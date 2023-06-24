a3c027c532ef785ffa591e372c77e32d574cdcb3
alexandrelam <alexandrelam@outlook.com>
2023-06-24 20:43:04 +0000

    Deploying to gh-pages from @ alexandrelam/blame-detective@a6b65cd6db1c8fb071b6991d1dfc10848c28e8ab 🚀

--- src/components/layouts/GlobalLayout.tsx
+++
@@ -1,7 +0,0 @@
-export function GlobalLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex flex-col h-screen max-h-screen	max-w-[1800px] px-4 m-auto overflow-hidden">
-      {children}
-    </div>
-  );
-}
--- src/hooks/useTypedContext.ts
+++
@@ -1,9 +0,0 @@
-import { Context, useContext } from "react";
-
-export function useTypedContext<T>(context: Context<T | null>) {
-  const contextValue = useContext(context);
-  if (contextValue === null) {
-    throw Error("Context has not been Provided!");
-  }
-  return contextValue;
-}
