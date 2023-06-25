20bf8fa3b7a1695291e5b1f8bb88564ad2968af9
Alexandre Lam <alexandre.lam@doctolib.com>
2023-06-25 12:41:53 +0200

    docs: updated patch note

--- src/components/PatchNoteModal.tsx
+++ src/components/PatchNoteModal.tsx
@@ -22,6 +22,64 @@ export function PatchNoteModal() {
           Patch Note 🚀
         </h2>
         <h3 className="font-medium text-lg mt-4 text-primary-content">
+          V0.2 Hextech Protobelt-01
+        </h3>
+        <span className="text-sm -mt-1 text-base-content block">
+          Release Date: June 25, 2023
+        </span>
+        <h4 className="font-bold mt-2 text-secondary-content">New ✨</h4>
+        <ul className="list-decimal mx-5 text-secondary-content">
+          <li>
+            Git Diff Enhancements: We have added a highly requested feature that
+            allows you to easily navigate and analyze changes in your Git
+            repositories. When performing a regex text search, the app now
+            highlights the lines in the Git diff that match your search
+            criteria. This makes it even more convenient to track specific
+            changes within your codebase.
+          </li>
+          <li>
+            Stay Up-to-Date: We understand the importance of keeping our users
+            informed about new releases and exciting updates. To achieve this,
+            we have introduced a new patch note component. This component will
+            provide you with timely information about the latest features,
+            improvements, and bug fixes in each release. Now, you can stay up to
+            date with the app's progress and never miss out on any important
+            announcements.
+          </li>
+          <li>
+            Improved Calendar Input Validation: Our calendar feature now
+            includes enhanced validation to ensure accurate and logical date
+            selection. We have implemented a validation mechanism that prevents
+            users from choosing a second date that falls in the past. This
+            enhancement eliminates any confusion or unintended selections,
+            making the calendar input more intuitive and error-free.
+          </li>
+        </ul>
+
+        <h4 className="font-bold mt-2 text-secondary-content">Bug fix 🐛</h4>
+        <ul className="list-decimal mx-5 text-secondary-content">
+          <li>
+            Default Theme Stability: We have addressed an issue that caused the
+            default theme to malfunction when launching the app for the first
+            time. The bug has been fixed, ensuring a seamless and visually
+            pleasing experience right from the start.
+          </li>
+          <li>
+            React Icons Warning Resolved: We have resolved a warning related to
+            the usage of icons in React. This fix eliminates any potential
+            issues or conflicts that may have occurred, resulting in a smoother
+            and error-free app performance.
+          </li>
+          <li>
+            Improved Context Organization: In our ongoing efforts to improve
+            code structure and maintainability, we have moved the context to
+            their own dedicated folder. This reorganization enhances the clarity
+            and organization of the app's architecture, making it easier for
+            developers to navigate and maintain the codebase.
+          </li>
+        </ul>
+        <span className="block w-full h-0.5 bg-base-300 my-6"></span>
+        <h3 className="font-medium text-lg mt-4 text-primary-content">
           V0.1 Nashor's tooth
         </h3>
         <span className="text-sm -mt-1 text-base-content block">
